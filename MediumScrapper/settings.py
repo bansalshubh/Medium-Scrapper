87229f28d7c7b1509754278c7dfe403f9e5fd56c
Shubham <bansalshubham.cse21@gmail.com>
2022-02-01 12:55:43 +0530

    edit

--- MediumScrapper/settings.py
+++ MediumScrapper/settings.py
@@ -119,11 +119,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
